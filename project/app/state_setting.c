a5dc34f2edec8c85d2ec26fab4bb551de7e3327b
zkwu2 <zkwu2@iflytek.com>
2024-10-08 14:42:06 +0800

    优化代码

--- project/app/state_setting.c
+++ project/app/state_setting.c
@@ -1,18 +1,30 @@
 #include "robot.h"
 #include "hw_timer.h"
 
+typedef struct{
+    int32_t id;
+    int32_t flag;
+}state_setting_t;
+
 int32_t state_setting(robot_state_t *self, int32_t event)
 {
+    state_setting_t *state = self->parm[STAT_PARM_NUM - 1];
     switch(event)
     {
         case EVE_STAT_ENTER:
             robot_state_show_enter();
+            state = os_mem_alloc(sizeof(state_setting_t));
+            self->parm[STAT_PARM_NUM - 1] = state;
+            state->id = 1001;
+            state->flag = 1;
             break;
         case EVE_STAT_EXIT:
             hw_timer_stop(TIME1);
+            os_mem_free(state);
             robot_state_show_exit();
             break;
         case EVE_UI_EVT1:
+            logi("id = %d, flag = %d\n", state->id, state->flag);
             robot_state_back((void *)10);
             break;
         case EVE_UI_EVT2:
